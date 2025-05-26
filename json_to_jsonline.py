f2833a693f89c6f261f8e0b20789c902a46dbfe0
Akshil Jain <115412236+AJ125000@users.noreply.github.com>
2025-05-26 16:47:25 +0530

    Update json-and-jsonline-interconversion.py

--- json_to_jsonline.py
+++ json_to_jsonline.py
@@ -1,5 +1,6 @@
 import jsonlines
 import json
+
 def json_to_jsonline(input_file, output_file):
 
     with jsonlines.open(output_file, mode='w') as writer:
@@ -8,13 +9,23 @@ def json_to_jsonline(input_file, output_file):
                 data = json.loads(line)
                 writer.write(data)
 
+def jsonline_to_json(input_file, output_file):
+    with open(output_file, 'w', encoding='utf-8') as writer:
+        with jsonlines.open(input_file, mode='r') as reader:
+            for obj in reader:
+                writer.write(json.dumps(obj, ensure_ascii=False) + '\n')                
+
 if __name__ == "__main__":
-    import argparse
 
-    parser = argparse.ArgumentParser(description="Convert JSON file to JSON Lines format.")
-    parser.add_argument("input_file", type=str, help="Path to the input JSON file.")
-    parser.add_argument("output_file", type=str, help="Path to the output JSON Lines file.")
-
-    args = parser.parse_args()
-
-    json_to_jsonline(args.input_file, args.output_file) 
+    user_input = input("Do you want to convert JSON to JSON Lines (j) or JSON Lines to JSON (jl)? (j/jl): ").strip().lower()
+    if user_input not in ['j', 'jl']:
+        print("Invalid input. Please enter 'j' for JSON to JSON Lines or 'jl' for JSON Lines to JSON.")
+        exit(1)
+    if user_input == 'j':
+        input_file = input("Enter the path to the input JSON file: ").strip()
+        output_file = input("Enter the path to the output JSON Lines file: ").strip()
+        json_to_jsonline(input_file, output_file)
+    elif user_input == 'jl':
+        input_file = input("Enter the path to the input JSON Lines file: ").strip()
+        output_file = input("Enter the path to the output JSON file: ").strip()
+        jsonline_to_json(input_file, output_file)    
